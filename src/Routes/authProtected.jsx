ebbb23b7a0b46d20dbfa87148cdbdbcabc9d82a9
urvashi17 <urvashi.shivinfotech@gmail.com>
2025-05-28 15:01:18 +0530

    change route in auth protected

--- src/Routes/authProtected.jsx
+++ src/Routes/authProtected.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Navigate, Route } from "react-router-dom";
 import { setAuthorization } from "../helpers/api_helper";
-import { LOGIN } from "../Api/ApiRoutes";
+import { LOGIN } from "./commonRoutes";
 
 const AuthProtected = (props) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
