eeba5f968e6f91566cd98d9620f807b40a9d95a8
urvashi17 <urvashi.shivinfotech@gmail.com>
2025-05-28 13:44:05 +0530

    change file name

--- src/Routes/allRoutes.jsx
+++ src/Routes/allRoutes.jsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 import Login from "../Pages/Authentication/Login";
 import Dashboard from "../Pages/Dashboard/Dashboard";
 import ForgetPasswordPage from "../Pages/Authentication/ForgotPassword";
-import { DASHBOARD, FORGOT_PASSWORD, LOGIN } from "./common";
+import { DASHBOARD, FORGOT_PASSWORD, LOGIN } from "./commonRoutes";
 
 const authProtectedRoutes = [
   { path: DASHBOARD, component: <Dashboard /> },
